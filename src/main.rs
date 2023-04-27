00fedfe295917ce043155809582e6e3073770975
Navya Mehta <navya.mehta@gmail.com>
2023-04-27 19:04:26 -0400

    Benchmarks and RustFMT

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use rand::prelude::IteratorRandom;
 use std::io::BufRead;
+use rayon::prelude::*;
 mod search;
 
-fn search_exhaustive<const N: usize>(all_data: &Vec<search::Vector<N>>, vector: &search::Vector<N>, top_k: i32) -> Vec<search::Vector<N>> {
+fn search_exhaustive<const N: usize>(all_data: &Vec<search::Vector<N>>, vector: &search::Vector<N>, top_k: i32) -> std::collections::HashSet<i32> {
     let enumerated_iter = all_data.iter().enumerate();
     let mut idx_sq_euc_dis: Vec<(usize, f32)> = enumerated_iter.map(|(i, can)| (i, can.sq_euc_dis(vector))).collect();
     idx_sq_euc_dis.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
-    let mut final_candidates: Vec<search::Vector<N>> = Vec::new();
+    // Return a set of IDs corresponding to the closest matches
+    let mut final_candidates = std::collections::HashSet::new();
     for i in 0..top_k as usize {
-        final_candidates.push(all_data[idx_sq_euc_dis[i].0])
+        final_candidates.insert(idx_sq_euc_dis[i].0 as i32);
     }
     return final_candidates
 }
@@ -31,31 +33,42 @@ fn load_raw_wiki_data<const N: usize>(
         idx_to_word_mapping.insert(cur_idx, word.to_owned());
         cur_idx += 1;
         // Parse the vector. Everything except the word on the line is the vector
-        let embedding: [f32; N] = data_on_line_iter.map(|s| s.parse::<f32>().unwrap()).collect::<Vec<_>>().try_into().unwrap();
+        let embedding: [f32; N] = data_on_line_iter.map(
+            |s| s.parse::<f32>().unwrap()).collect::<Vec<_>>().try_into().unwrap();
         all_data.push(search::Vector(embedding));
     }
 }
 
-fn build_benchmark_and_visualize_index<const N: usize, const MAX_N: usize>(
+fn search_approximate_as_hashset<const N: usize>(
+    index: &search::ANNIndex<N>, vector: search::Vector<N>, top_k: i32
+) -> std::collections::HashSet<i32> {
+    let nearby_idx_and_distance = index.search_approximate(vector, top_k);
+    let mut id_hashset = std::collections::HashSet::new();
+    for &(idx, _) in nearby_idx_and_distance.iter() {
+        id_hashset.insert(idx);
+    }
+    return id_hashset
+}
+
+fn build_benchmark_and_visualize_index<const N: usize>(
     my_input_data: &Vec<search::Vector<N>>,
     word_to_idx_mapping: &std::collections::HashMap<String, usize>,
     idx_to_word_mapping: &std::collections::HashMap<usize, String>,
-    num_trees: i32, max_node_size: i32, top_k: i32, words_to_visualize: &Vec<String>,
-    highest_dim_input_data: &Vec<search::Vector<MAX_N>>,
-    print_all_distances: bool) {
+    num_trees: i32, max_node_size: i32, top_k: i32,
+    indices_of_interest: &Vec<i32>, words_to_visualize: &Vec<String>) -> Vec<std::collections::HashSet<i32>> {
     println!("dimensions={}, num_trees={}, max_node_size={}, top_k={}", N, num_trees, max_node_size, top_k);
     // Build the index
     let start = std::time::Instant::now();
     let my_ids: Vec<i32> = (0..my_input_data.len() as i32).collect();
-    let index = search::ANNIndex::<N>::build_an_index(
+    let index = search::ANNIndex::<N>::build_index(
         num_trees, max_node_size, &my_input_data, &my_ids);
     let duration = start.elapsed();
     println!("Build ANN index in {}-D in {:?}", N, duration);
     // Benchmark it with 1000 sequential queries
-    let sample_idx: Vec<usize> = (0..my_input_data.len()).choose_multiple(&mut rand::thread_rng(), 1000);
+    let sample_idx: Vec<i32> = (0..my_input_data.len() as i32).choose_multiple(&mut rand::thread_rng(), 1000);
     let mut search_vectors: Vec<search::Vector<N>> = Vec::new();
     for idx in sample_idx {
-        search_vectors.push(my_input_data[idx]);
+        search_vectors.push(my_input_data[idx as usize]);
     }
     let start = std::time::Instant::now();
     for i in 0..1000 {
@@ -63,24 +76,6 @@ fn build_benchmark_and_visualize_index<const N: usize, const MAX_N: usize>(
     }
     let duration = start.elapsed() / 1000;
     println!("Bulk ANN-search in {}-D has average time {:?}", N, duration);
-    // Calculate the average distance. The distances are computed against the full 300-D embeddings
-    // and not the reduced dimensionality in the index.
-    let mut distances = Vec::new();
-    for (i, &vector) in my_input_data.iter().enumerate() {
-        // Ignore the distance here since we must compute it against the full 300-D embedding
-        let id_and_sq_dist = index.search_approximate(vector, top_k);
-        let mut total_dist = 0.0;
-        for &(id, _) in id_and_sq_dist.iter() {
-            total_dist += highest_dim_input_data[i].sq_euc_dis(&highest_dim_input_data[id as usize]).sqrt()
-        }
-        distances.push(total_dist / id_and_sq_dist.len() as f32);
-    }
-    let total_dist: f32 = distances.iter().sum();
-    let average_dist =  total_dist / distances.len() as f32;
-    println!("Average Euclidean Distance = {}", average_dist);
-    if print_all_distances {
-        println!("{:?}", distances)
-    }
     // Visualize the results for some words
     for word in words_to_visualize.iter() {
         println!("Currently visualizing {}", word);
@@ -88,16 +83,52 @@ fn build_benchmark_and_visualize_index<const N: usize, const MAX_N: usize>(
         let embedding = my_input_data[word_index];
         let nearby_idx_and_distance = index.search_approximate(embedding, top_k);
         for &(idx, distance) in nearby_idx_and_distance.iter() {
-            println!("{}, sq distance={}", idx_to_word_mapping[&(idx as usize)], distance);
+            println!("{}, distance={}", idx_to_word_mapping[&(idx as usize)], distance.sqrt());
         }
     }
+    // For the indices of interest, find the top_k neighbours and return that data
+    let start = std::time::Instant::now(); 
+    let index_results = indices_of_interest.par_iter().map(
+        | &idx | search_approximate_as_hashset(&index, my_input_data[idx as usize], top_k)).collect();
+    let duration = start.elapsed();
+    println!("Collected sample of index quality in {:?}", duration);
+    return index_results;
+}
+
+fn calculate_metrics_on_index_result<const N: usize>(
+    all_embedding_data: &Vec<search::Vector<N>>, 
+    exhaustive_results: &Vec<std::collections::HashSet<i32>>,
+    index_results: &Vec<std::collections::HashSet<i32>>
+) {
+    // We wish to calculate the average Euclidean distance and the recall@k with k=20. However, running
+    // this for all 1 mil embeddings will make the code run long. We thus take a sample size of 30k out of
+    // the total 1 mil to get an estimate for these values. The distances are computed against the full 
+    // 300-D embeddings and not the reduced dimensionality in the index.
+    let start = std::time::Instant::now();
+    let mut total_euc_dist = 0.0;
+    let mut total_recall_pct = 0.0;
+    for (i, neighbours) in index_results.iter().enumerate() {
+        // Ignore the distance here since we must compute it against the full 300-D embedding
+        let mut sum_of_dist_to_neighbours = 0.0;
+        let mut num_matches_with_brute_results = 0.0;
+        for &neighbour_id in neighbours.iter() {
+            sum_of_dist_to_neighbours += all_embedding_data[i].sq_euc_dis(&all_embedding_data[neighbour_id as usize]).sqrt();
+            if exhaustive_results[i].contains(&neighbour_id) {
+                num_matches_with_brute_results += 1.0;
+            }
+        }
+        total_euc_dist += sum_of_dist_to_neighbours / neighbours.len() as f32;
+        total_recall_pct += num_matches_with_brute_results / neighbours.len() as f32;
+    }
+    let average_dist =  total_euc_dist / exhaustive_results.len() as f32;
+    let average_recall = total_recall_pct / exhaustive_results.len() as f32;
+    let duration = start.elapsed();
+    println!("Average Euclidean Distance = {}, Average Recall% = {} in {:?}", average_dist, average_recall, duration);
 }
 
 fn main() {
     const DIM: usize = 300;
-    const NUM_TREES: i32 = 3;
     const TOP_K: i32 = 20;
-    const MAX_NODE_SIZE : i32 = 15;
     // Parse the data from wiki-news
     let start = std::time::Instant::now();
     let mut my_input_data: Vec<search::Vector<DIM>> = Vec::new();
@@ -110,46 +141,38 @@ fn main() {
     println!("Parsed {} vectors in {}-D in {:?}", my_input_data.len(), DIM, duration);
     // Try the naive exact-search for TOP_K elements
     let start = std::time::Instant::now();
-    let brute_results = search_exhaustive::<DIM>(&my_input_data, &my_input_data[0], TOP_K);
+    search_exhaustive::<DIM>(&my_input_data, &my_input_data[0], TOP_K);
     let duration = start.elapsed();
-    println!("Found {} vectors via brute-search in {}-D in {:?}", brute_results.len(), DIM, duration);
+    println!("Found vectors via brute-search in {}-D in {:?}", DIM, duration);
+    // Take 1000 random vectors from the input data and find its TOP_K nearest neighbors using the
+    // exhaustive/brute-force approach. This allows us to calculate recall for our implementations.
+    // This is a list of randomly chosen indices from our main embedding set - we use a subset to
+    // estimate our metrics due to the computation cost.
+    let start = std::time::Instant::now();
+    rayon::ThreadPoolBuilder::new().num_threads(5).build_global().unwrap();
+    let sample_idx: Vec<i32> = (0..my_input_data.len() as i32).choose_multiple(&mut rand::thread_rng(), 1000);
+    // Make a vector of hashsets where hashset at position i represents the exhaustive nearest neighbours
+    // for the embedding at position idx in my_input_data, where idx is at position i in sample_idx.
+    let exhaustive_results: Vec<std::collections::HashSet<i32>> = sample_idx.par_iter().map(
+        |&idx| search_exhaustive::<DIM>(&my_input_data, &my_input_data[idx as usize], TOP_K)).collect();
+    let duration = start.elapsed();
+    println!("Found exhaustive neighbors for sample to calculate recall in {:?}", duration);
     // Main parameters
     let input_words = ["river", "war", "love", "education"];
     let words_to_visualize: Vec<String> = input_words.into_iter().map(|x| x.to_owned()).collect();
-    build_benchmark_and_visualize_index::<DIM, DIM>(
+    let index_results = build_benchmark_and_visualize_index::<DIM>(
         &my_input_data, &word_to_idx_mapping, &idx_to_word_mapping,
-        NUM_TREES, MAX_NODE_SIZE, TOP_K, &words_to_visualize, &my_input_data, true);
-    // See how run-times change based on parameters
-    const DIM_60: usize = 60;
-    let mut data_dim_60: Vec<search::Vector<DIM_60>> = Vec::new();
-    const DIM_150: usize = 150;
-    let mut data_dim_150: Vec<search::Vector<DIM_150>> = Vec::new();
-    for vector in my_input_data.iter() {
-        let dim_60: [f32; DIM_60] = (vector.0)[0..DIM_60].try_into().unwrap();
-        let dim_150: [f32; DIM_150] = (vector.0)[0..DIM_150].try_into().unwrap();
-        data_dim_60.push(search::Vector(dim_60));
-        data_dim_150.push(search::Vector(dim_150));
-    }
+        3, 15, TOP_K, &sample_idx, &words_to_visualize);
+    calculate_metrics_on_index_result::<DIM>(&my_input_data, &exhaustive_results, &index_results);
+    // Try some other parameters. New values for max_node_size, num_trees at dim=300. See how we can make it
+    // better in its accuracy/ quality.
     let no_words: Vec<String> = Vec::new();
     for num_trees in [3, 9, 15] {
         for max_node_size in [5, 15, 30] {
-            build_benchmark_and_visualize_index::<DIM_60, DIM>(
-                &data_dim_60, &word_to_idx_mapping, &idx_to_word_mapping,
-                num_trees, max_node_size, TOP_K, &no_words, &my_input_data, false);
-        }
-    }
-    for num_trees in [3, 9, 15] {
-        for max_node_size in [5, 15, 30] {
-            build_benchmark_and_visualize_index::<DIM_150, DIM>(
-                &data_dim_150, &word_to_idx_mapping, &idx_to_word_mapping,
-                num_trees, max_node_size, TOP_K, &no_words, &my_input_data, false);
-        }
-    }
-    for num_trees in [3, 9, 15] {
-        for max_node_size in [5, 15, 30] {
-            build_benchmark_and_visualize_index::<DIM, DIM>(
+            let index_results = build_benchmark_and_visualize_index::<DIM>(
                 &my_input_data, &word_to_idx_mapping, &idx_to_word_mapping,
-                num_trees, max_node_size, TOP_K, &no_words, &my_input_data, false);
+                num_trees, max_node_size, TOP_K, &sample_idx, &no_words);
+            calculate_metrics_on_index_result::<DIM>(&my_input_data, &exhaustive_results, &index_results);
         }
     }
 }
--- src/search.rs
+++ src/search.rs
@@ -14,7 +14,7 @@ impl<const N: usize> HyperPlane<N> {
 }
 
 #[derive(Copy, Clone)]
-pub struct Vector<const N: usize> (pub [f32; N]);
+pub struct Vector<const N: usize>(pub [f32; N]);
 impl<const N: usize> Vector<N> {
     pub fn subtract_from(&self, vector: &Vector<N>) -> Vector<N> {
         let mapped_iter = self.0.iter().zip(vector.0).map(|(a, b)| b - a);
@@ -31,8 +31,8 @@ impl<const N: usize> Vector<N> {
         return zipped_iter.map(|(a, b)| a * b).sum::<f32>();
     }
     pub fn to_hashkey(&self) -> HashKey<N> {
-        // f32 in Rust doesn't implement hash - we use byte representation to deduplicate which is unsafe in that it
-        // cannot differentiate ~16 mil representations of NaN under IEEE-754 but safe for our demonstration
+        // f32 in Rust doesn't implement hash - we use byte to deduplicate which is unsafe in that it
+        // cannot differentiate ~16 mil ways NaN is written under IEEE-754 but safe for us
         let bit_iter = self.0.iter().map(|a| a.to_bits());
         let u32_data: [u32; N] = bit_iter.collect::<Vec<_>>().try_into().unwrap();
         return HashKey::<N>(u32_data);
@@ -43,46 +43,69 @@ impl<const N: usize> Vector<N> {
     }
 }
 
-
-enum Node<const N: usize> { Inner(Box<InnerNode<N>>), Leaf(Box<LeafNode<N>>) }
+enum Node<const N: usize> {
+    Inner(Box<InnerNode<N>>),
+    Leaf(Box<LeafNode<N>>),
+}
 struct LeafNode<const N: usize>(Vec<usize>);
 struct InnerNode<const N: usize> {
     hyperplane: HyperPlane<N>,
     left_node: Node<N>,
     right_node: Node<N>,
 }
-pub struct ANNIndex<const N: usize> { trees: Vec<Node<N>>, ids: Vec<i32>, values: Vec<Vector<N>> }
+pub struct ANNIndex<const N: usize> {
+    trees: Vec<Node<N>>,
+    ids: Vec<i32>,
+    values: Vec<Vector<N>>,
+}
 impl<const N: usize> ANNIndex<N> {
-    fn build_hyperplane_bwn_two_random_points(indexes_of_interest: &Vec<usize>, all_vectors: &Vec<Vector<N>>) -> (HyperPlane<N>, Vec<usize>, Vec<usize>) {
-        let sample: Vec<_> = indexes_of_interest.choose_multiple(&mut rand::thread_rng(), 2).collect();
+    fn build_hyperplane(
+        indexes: &Vec<usize>,
+        all_vectors: &Vec<Vector<N>>,
+    ) -> (HyperPlane<N>, Vec<usize>, Vec<usize>) {
+        let sample: Vec<_> = indexes
+            .choose_multiple(&mut rand::thread_rng(), 2)
+            .collect();
         // implicit Cartesian eq for hyperplane n * (x - x_0) = 0. n (normal vector) is the coefs x_1 to x_n
         let coefficients = all_vectors[*sample[0]].subtract_from(&all_vectors[*sample[1]]);
         let point_on_plane = all_vectors[*sample[0]].avg(&all_vectors[*sample[1]]);
-        let constant = - coefficients.dot_product(&point_on_plane);
-        let mut above: Vec<usize> = Vec::new();
-        let mut below: Vec<usize> = Vec::new();
-        let hyperplane = HyperPlane::<N> { coefficients: coefficients, constant: constant };
-        for &id in indexes_of_interest.iter() {
-            if hyperplane.point_is_above(&all_vectors[id]) { above.push(id) } else { below.push(id) };
+        let constant = -coefficients.dot_product(&point_on_plane);
+        let (mut above, mut below) = (Vec::new(), Vec::new());
+        let hyperplane = HyperPlane::<N> {
+            coefficients: coefficients,
+            constant: constant,
+        };
+        for &id in indexes.iter() {
+            if hyperplane.point_is_above(&all_vectors[id]) {
+                above.push(id)
+            } else {
+                below.push(id)
+            };
         }
         return (hyperplane, above, below);
     }
 
-    fn build_a_tree(max_size_of_node: i32, indexes_of_interest: &Vec<usize>, all_vectors: &Vec<Vector<N>>) -> Node<N> {
-        if indexes_of_interest.len() <= (max_size_of_node as usize) {
-            return Node::Leaf(Box::new(LeafNode::<N>(indexes_of_interest.clone())));
+    fn build_a_tree(max_size: i32, indexes: &Vec<usize>, all_vecs: &Vec<Vector<N>>) -> Node<N> {
+        if indexes.len() <= (max_size as usize) {
+            return Node::Leaf(Box::new(LeafNode::<N>(indexes.clone())));
         }
-        let (hyperplane, above, below) = Self::build_hyperplane_bwn_two_random_points(indexes_of_interest, all_vectors);
-        let node_above = Self::build_a_tree(max_size_of_node, &above, all_vectors);
-        let node_below = Self::build_a_tree(max_size_of_node, &below, all_vectors);
+        let (hyperplane, above, below) = Self::build_hyperplane(indexes, all_vecs);
+        let node_above = Self::build_a_tree(max_size, &above, all_vecs);
+        let node_below = Self::build_a_tree(max_size, &below, all_vecs);
         return Node::Inner(Box::new(InnerNode::<N> {
-            hyperplane: hyperplane, left_node: node_below, right_node: node_above}));
+            hyperplane: hyperplane,
+            left_node: node_below,
+            right_node: node_above,
+        }));
     }
 
-    fn deduplicate_vector_list(
-        vectors: &Vec<Vector<N>>, ids: &Vec<i32>, dedup_vectors: &mut Vec<Vector<N>>, 
-        ids_of_dedup_vectors: &mut Vec<i32>) {
-        let mut hashes_seen: std::collections::HashSet<HashKey<N>> = std::collections::HashSet::new();
+    fn deduplicate(
+        vectors: &Vec<Vector<N>>,
+        ids: &Vec<i32>,
+        dedup_vectors: &mut Vec<Vector<N>>,
+        ids_of_dedup_vectors: &mut Vec<i32>,
+    ) {
+        let mut hashes_seen = std::collections::HashSet::new();
         for i in 1..vectors.len() {
             let hash_key = vectors[i].to_hashkey();
             if !hashes_seen.contains(&hash_key) {
@@ -93,39 +116,53 @@ impl<const N: usize> ANNIndex<N> {
         }
     }
 
-    pub fn build_an_index(num_trees: i32, max_size_of_node: i32, vectors: &Vec<Vector<N>>, vector_ids: &Vec<i32>) -> ANNIndex<N> {
-        let mut unique_vectors = Vec::new();
-        let mut ids_of_unique_vectors = Vec::new();
-        Self::deduplicate_vector_list(vectors, vector_ids, &mut unique_vectors, &mut ids_of_unique_vectors);
-        // Trees hold an index into the [unique_vectors] list which is not necessarily its id, if duplicates existed
-        let all_indexes_unique_vectors: Vec<usize> = (0..unique_vectors.len()).collect();
-        let mut trees: Vec<Node<N>> = Vec::new();
+    pub fn build_index(
+        num_trees: i32,
+        max_size: i32,
+        vecs: &Vec<Vector<N>>,
+        vec_ids: &Vec<i32>,
+    ) -> ANNIndex<N> {
+        let (mut unique_vecs, mut ids, mut trees) = (Vec::new(), Vec::new(), Vec::new());
+        Self::deduplicate(vecs, vec_ids, &mut unique_vecs, &mut ids);
+        // Trees hold an index into the [unique_vecs] list which is not necessarily its id, if duplicates existed
+        let all_indexes: Vec<usize> = (0..unique_vecs.len()).collect();
         for _ in 0..num_trees {
-            trees.push(Self::build_a_tree(max_size_of_node, &all_indexes_unique_vectors, &unique_vectors))
+            let tree = Self::build_a_tree(max_size, &all_indexes, &unique_vecs);
+            trees.push(tree);
         }
-        return ANNIndex::<N> { trees: trees, ids: ids_of_unique_vectors, values: unique_vectors };
+        return ANNIndex::<N> {
+            trees: trees,
+            ids: ids,
+            values: unique_vecs,
+        };
     }
 
-    fn get_candidates_per_tree(vector: Vector<N>, num_candidates: i32, tree: &Node<N>, candidates: &mut std::collections::HashSet<usize>) -> i32 {
-        // We take everything in the leaf node we end up with. If we still need candidates, we take closer ones from the alternate subtree
+    fn tree_result(
+        vector: Vector<N>,
+        n: i32,
+        tree: &Node<N>,
+        candidates: &mut std::collections::HashSet<usize>,
+    ) -> i32 {
+        // We take everything in the leaf node. If we still need, we take ones from the alternate subtree
         match tree {
             Node::Leaf(box_leaf) => {
                 let leaf_values = &(**box_leaf).0;
-                let num_candidates_found = std::cmp::min(num_candidates as usize, leaf_values.len());
+                let num_candidates_found = std::cmp::min(n as usize, leaf_values.len());
                 for i in 0..num_candidates_found {
                     candidates.insert(leaf_values[i]);
                 }
                 return num_candidates_found as i32;
             }
             Node::Inner(box_inner) => {
-                let (correct_tree, backup_tree) = if (*box_inner).hyperplane.point_is_above(&vector) {
+                let (correct_tree, backup_tree) = if (*box_inner).hyperplane.point_is_above(&vector)
+                {
                     (&(*box_inner).right_node, &(*box_inner).left_node)
                 } else {
                     (&(*box_inner).left_node, &(*box_inner).right_node)
                 };
-                let mut fetched = Self::get_candidates_per_tree(vector, num_candidates, correct_tree, candidates);
-                if fetched < num_candidates {
-                    fetched += Self::get_candidates_per_tree(vector, num_candidates - fetched, backup_tree, candidates);
+                let mut fetched = Self::tree_result(vector, n, correct_tree, candidates);
+                if fetched < n {
+                    fetched += Self::tree_result(vector, n - fetched, backup_tree, candidates);
                 };
                 return fetched;
             }
@@ -133,11 +170,14 @@ impl<const N: usize> ANNIndex<N> {
     }
 
     pub fn search_approximate(&self, vector: Vector<N>, top_k: i32) -> Vec<(i32, f32)> {
-        let mut candidates: std::collections::HashSet<usize> = std::collections::HashSet::new();
+        let mut candidates = std::collections::HashSet::new();
         for tree in self.trees.iter() {
-            Self::get_candidates_per_tree(vector, top_k, tree, &mut candidates);
+            Self::tree_result(vector, top_k, tree, &mut candidates);
         }
-        let mut idx_sq_euc_dis: Vec<(usize, f32)> = candidates.iter().map(|&idx| (idx, self.values[idx].sq_euc_dis(&vector))).collect();
+        let mut idx_sq_euc_dis: Vec<(usize, f32)> = candidates
+            .iter()
+            .map(|&idx| (idx, self.values[idx].sq_euc_dis(&vector)))
+            .collect();
         idx_sq_euc_dis.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
         let mut final_candidates: Vec<(i32, f32)> = Vec::new();
         let num_candidates_to_return = std::cmp::min(top_k as usize, candidates.len());
